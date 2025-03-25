1246ee45febe8b966721d58f6be023ecc7a27ee2
Dinesh Kharah <dineshkharahcodes@gmail.com>
2025-03-25 20:17:06 +0530

    changes background color

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -45,7 +45,7 @@ const Login = () => {
 
 
     return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="flex items-center justify-center min-h-screen bg-gray-900">
             <div className="bg-white p-8 rounded-lg shadow-md w-96">
                 <h2 className="text-2xl font-bold text-center mb-6">Login</h2>
 
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -50,7 +50,7 @@ const Signup = () => {
     };
 
     return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="flex items-center justify-center min-h-screen bg-gray-900">
             <div className="bg-white p-8 rounded-lg shadow-md w-96">
                 <h2 className="text-2xl font-bold text-center mb-6">Sign Up</h2>
 
